bac069dd078e231839abad6326ad4d15313e2d05
Murat GÖKTAŞ <77151500+muratgoktas@users.noreply.github.com>
2021-05-16 21:23:00 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult KisiselBilgilerim()
+        {
+            return View();
+        }
+        public ActionResult EgitimBilgilerim()
+        {
+            return View();
+        }
+        public ActionResult IsBilgilerim()
+        {
+            return View();
+        }
+        public ActionResult iletisimBilgilerim()
+        {
+            return View();
+            
+        }
+        public ActionResult DenemeSayfasi()
+        {
+            return View();
+
+        }
+
+    }
+}
